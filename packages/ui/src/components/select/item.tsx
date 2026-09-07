96354bc2a2aed072c6b783c2cf3af48d00bacc13
arshad <arshadpyaseen@gmail.com>
2026-09-07 14:43:34 +0530

    select: align the item highlight with the trigger's edges

--- packages/ui/src/components/select/item.tsx
+++ packages/ui/src/components/select/item.tsx
@@ -12,7 +12,7 @@ export function Item({ className, ...props }: SelectItemProps) {
         "cursor-default scroll-my-1 rounded-md ps-2.5 pe-4 outline-hidden select-none",
         "py-[calc((var(--anchor-height,--spacing(8))-(--spacing(6)))/2)]",
         "text-sm/6 text-foreground group-data-[side=none]:pe-10",
-        "data-highlighted:before:absolute data-highlighted:before:inset-x-1 data-highlighted:before:inset-y-[-0.5px] data-highlighted:before:-z-10 data-highlighted:before:rounded-md data-highlighted:before:bg-neutral-200 data-highlighted:before:content-[''] dark:data-highlighted:before:bg-neutral-800/50",
+        "data-highlighted:before:absolute data-highlighted:before:inset-x-[calc(--spacing(1)-1px)] data-highlighted:before:-inset-y-px data-highlighted:before:-z-10 data-highlighted:before:rounded-[calc(var(--radius-md)+1px)] data-highlighted:before:bg-neutral-200 data-highlighted:before:content-[''] dark:data-highlighted:before:bg-neutral-800/50",
         "data-disabled:pointer-events-none data-disabled:opacity-50",
         className,
       )}
