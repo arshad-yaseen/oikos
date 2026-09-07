4f4d6ee23c7c4553f472072860308174650f003d
arshad <arshadpyaseen@gmail.com>
2026-09-07 14:45:58 +0530

    update

--- apps/web/src/content/ui/components/button/as-link.tsx
+++ apps/web/src/content/ui/components/button/as-link.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import { Button } from "@arshad.fyi/ui/components/button";
 import { Icon } from "@arshad.fyi/ui/components/icon";
+import { Route } from "next";
 
 export function AsLink() {
   return (
-    <Button color="neutral" render={<Link href="/ui" />}>
+    <Button color="neutral" render={<Link href={"/ui" as Route} />}>
       Introduction
       <Icon name="ArrowUpRight" />
     </Button>
